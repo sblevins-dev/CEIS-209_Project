0eceaef65358fac97dba754b6bc8ae52c7b5a7fa
Stephen Blevins <scblevins27@gmail.com>
2021-12-03 23:54:35 -0500

    Initial commit

---
+++ Blevins_CourseProject_part2/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿
+namespace Blevins_CourseProject_part2
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AddButton = new System.Windows.Forms.Button();
+            this.RemoveButton = new System.Windows.Forms.Button();
+            this.DisplayButton = new System.Windows.Forms.Button();
+            this.PrintPaychecksButton = new System.Windows.Forms.Button();
+            this.EmployeesListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // AddButton
+            // 
+            this.AddButton.Location = new System.Drawing.Point(24, 38);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(100, 38);
+            this.AddButton.TabIndex = 0;
+            this.AddButton.Text = "Add";
+            this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // RemoveButton
+            // 
+            this.RemoveButton.Location = new System.Drawing.Point(160, 38);
+            this.RemoveButton.Name = "RemoveButton";
+            this.RemoveButton.Size = new System.Drawing.Size(111, 38);
+            this.RemoveButton.TabIndex = 1;
+            this.RemoveButton.Text = "Remove";
+            this.RemoveButton.UseVisualStyleBackColor = true;
+            this.RemoveButton.Click += new System.EventHandler(this.RemoveButton_Click);
+            // 
+            // DisplayButton
+            // 
+            this.DisplayButton.Location = new System.Drawing.Point(307, 38);
+            this.DisplayButton.Name = "DisplayButton";
+            this.DisplayButton.Size = new System.Drawing.Size(100, 38);
+            this.DisplayButton.TabIndex = 2;
+            this.DisplayButton.Text = "Display";
+            this.DisplayButton.UseVisualStyleBackColor = true;
+            this.DisplayButton.Click += new System.EventHandler(this.DisplayButton_Click);
+            // 
+            // PrintPaychecksButton
+            // 
+            this.PrintPaychecksButton.Location = new System.Drawing.Point(443, 38);
+            this.PrintPaychecksButton.Name = "PrintPaychecksButton";
+            this.PrintPaychecksButton.Size = new System.Drawing.Size(191, 38);
+            this.PrintPaychecksButton.TabIndex = 3;
+            this.PrintPaychecksButton.Text = "Print Paychecks";
+            this.PrintPaychecksButton.UseVisualStyleBackColor = true;
+            this.PrintPaychecksButton.Click += new System.EventHandler(this.PrintPaychecksButton_Click);
+            // 
+            // EmployeesListBox
+            // 
+            this.EmployeesListBox.FormattingEnabled = true;
+            this.EmployeesListBox.ItemHeight = 29;
+            this.EmployeesListBox.Location = new System.Drawing.Point(24, 124);
+            this.EmployeesListBox.Name = "EmployeesListBox";
+            this.EmployeesListBox.Size = new System.Drawing.Size(813, 323);
+            this.EmployeesListBox.TabIndex = 4;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(859, 482);
+            this.Controls.Add(this.EmployeesListBox);
+            this.Controls.Add(this.PrintPaychecksButton);
+            this.Controls.Add(this.DisplayButton);
+            this.Controls.Add(this.RemoveButton);
+            this.Controls.Add(this.AddButton);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
+            this.Name = "MainForm";
+            this.Text = "Payroll System";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.Button RemoveButton;
+        private System.Windows.Forms.Button DisplayButton;
+        private System.Windows.Forms.Button PrintPaychecksButton;
+        private System.Windows.Forms.ListBox EmployeesListBox;
+    }
+}
+
